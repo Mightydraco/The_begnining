a94c9fcf03ae4731eeebb7b5dc6f210d62c42ffd
Mightydraco <101245704+Mightydraco@users.noreply.github.com>
2022-03-14 12:29:28 +0100

    segona classe de llenguatge
    les coses que vam fer a la segona classe

--- Godot.gd
+++ Godot.gd
@@ -1,5 +1,5 @@
-extends Sprite
 
+extends Area2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,11 +9,22 @@ var direccio = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rotation_degrees = 90
-	position = Vector2(100,150)
+	position = Vector2(0,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += direccio * velocitat * delta
+	position += direccio.normalized() * velocitat * delta
+	direccio = Vector2(0,0)
 	if Input.is_action_pressed("mou_dreta"):
-		direccio = Vector2(1,0)
+		direccio += Vector2(1,0)
+	if Input.is_action_pressed("mou_abaix"):
+		direccio += Vector2(0,1)
+	if Input.is_action_pressed("mou_adalt"):
+		direccio += Vector2(0,-1)
+	if Input.is_action_pressed("mou_esquerra"):
+		direccio += Vector2(-1,0)
+
+
+func _on_Personatge_area_entered(area):
+	modulate = Color(1,0,0)
