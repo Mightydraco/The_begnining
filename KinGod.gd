91b1016172ad4728011b69189505bf0e0182849f
Mightydraco <101245704+Mightydraco@users.noreply.github.com>
2022-03-18 12:27:11 +0100

    character creation time

---
+++ KinGod.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var velocitat_base = 200
+var velocitat = Vector2.ZERO
+var gravetat = Vector2.DOWN * 980
+var salt = Vector2.UP * 500
+var salts_disponibles = 2
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	velocitat.x = 0
+	if salts_disponibles <= 0 and is_on_floor():
+		salts_disponibles = 2
+	if Input.is_action_pressed("mou_dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("mou_esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("mou_adalt") and salts_disponibles > 0:
+		velocitat += salt
+		salts_disponibles -= 1
+	velocitat += gravetat * delta
+	velocitat = move_and_slide(velocitat,Vector2.UP)
